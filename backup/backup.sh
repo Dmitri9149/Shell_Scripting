319f8f3ff2fab420ca71dcea2931c91c462f7b40
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-05 15:15:09 +0200

    some development, vars are used#

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# This bash script is used to backup a user's directory to /tmp/.
+# This bash script is used to backup a user's /home/idrisdirectory 
+# to /tmp/.  (if the user has the idris directory, of course)
+# later we will change to more general functionality
 
+user=$(whoami)
+date=/home/${user}/idris
+output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 
-tar -czf /tmp/myhome_directory.tar.gz /home/dmitri/idris
+tar -czf ${output} ${input}
+
+echo "Backup of $input completed! Details about the output backup file:"
+ls -l $output
