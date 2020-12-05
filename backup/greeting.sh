fa3a26c49a9f7af76d19e27c584111df7019ddb3
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-05 14:58:39 +0200

    bugs eliminated

--- backup/greeting.sh
+++ backup/greeting.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-greeting = "Welcome"
-user = $(whoami)
-day = $(date +A%)
-folder = $(pwd)
+greeting="Welcome"
+user=$(whoami)
+day=$(date +%A)
+folder=$(pwd)
 
 echo "$greeting back $user! Today is $day and you are working in $folder ."
 echo "Your bash version is $BASH_VERSION and not all is still lost."
