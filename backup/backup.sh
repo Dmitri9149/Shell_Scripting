6979ef7d3eafcd242f06aceb6dd3d7ae1a3c341d
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-06 15:30:08 +0200

    number of files and directories displayed

--- backup/backup.sh
+++ backup/backup.sh
@@ -8,6 +8,21 @@ user=$(whoami)
 input=/home/$user/idris/
 output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 
+## Reports a total number of files in directory
+function total_files {
+    find $1 -type f | wc -l
+}
+
+# Reports a total number of directories
+function total_directories {
+    find $1 -type d | wc -l
+}
+
+echo -n "Files to be included: "
+total_files $input
+echo -n "directories to be included: "
+total_directories $input
+
 tar -czf $output $input 2> /dev/null
 
 echo "Backup of $input completed! Details about the output backup file:"
