4b0aeb3429c3d671f4e1fbebaa50d0e3a9bb17e0
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-08 13:18:25 +0200

    for loops to backup multiple dirs is added

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,61 +1,68 @@
 #!/bin/bash
 
-# This bash script is used to backup a user's /home/.. directory 
+# This bash script is used to backup a user's /home/user/... subdirectories 
+## (which are given as script parameters)
 # to /tmp/. 
 
-user=$(whoami)
+function backup {
+        user=$(whoami)
 
-if [ -z $1 ]; then                                                                                                                                                                                                                                                             
-        echo "No parameters."
-        exit 1                                                                                                                                                                                                                                                         
-else                                                                                                                                                                                                                                                                           
-        if [ ! -d "/home/$user/$1" ]; then                                                                                                                                                                                                                                           
-                echo "Requested /home/$user/$1 user home directory doesn't exist."                                                                                                                                                                                                         
+        if [ -z $1 ]; then                                                                                                                                                                                                                                                             
+                echo "No parameters."
                 exit 1                                                                                                                                                                                                                                                         
-        fi                                                                                                                                                                                                                                                                     
-        user=$(whoami)                                                                                                                                                                                                                                                               
-fi 
+        else                                                                                                                                                                                                                                                                           
+                if [ ! -d "/home/$user/$1" ]; then                                                                                                                                                                                                                                           
+                        echo "Requested /home/$user/$1 user home directory doesn't exist."                                                                                                                                                                                                         
+                        exit 1                                                                                                                                                                                                                                                         
+                fi                                                                                                                                                                                                                                                                     
+                user=$(whoami)                                                                                                                                                                                                                                                               
+        fi 
 
-input=/home/$user/$1
-output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
+        input=/home/$user/$1
+        output=/tmp/${user}_$1_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 
-## Reports a total number of files in directory
-function total_files {
-    find $1 -type f | wc -l
+        ## Reports a total number of files in directory
+        function total_files {
+        find $1 -type f | wc -l
+        }
+
+        # Reports a total number of directories
+        function total_directories {
+        find $1 -type d | wc -l
+        }
+
+        function total_archived_directories {
+                tar -tzf $1 | grep  /$ | wc -l
+        }
+
+        function total_archived_files {
+                tar -tzf $1 | grep -v /$ | wc -l
+        }
+
+
+
+        tar -czf $output $input 2> /dev/null
+
+        src_files=$( total_files $input )
+        src_directories=$( total_directories $input )
+
+        arch_files=$( total_archived_files $output )
+        arch_directories=$( total_archived_directories $output )
+
+        echo "Files to be included: $src_files"
+        echo "Directories to be included: $src_directories"
+        echo "Files archived: $arch_files"
+        echo "Directories archived: $arch_directories"
+
+        if [ $src_files -eq $arch_files ]; then
+                echo "Backup of $input completed!"
+                echo "Details about the output backup file:"
+                ls -l $output
+        else
+                echo "Backup of $input failed!"
+        fi
 }
 
-# Reports a total number of directories
-function total_directories {
-    find $1 -type d | wc -l
-}
-
-function total_archived_directories {
-        tar -tzf $1 | grep  /$ | wc -l
-}
-
-function total_archived_files {
-        tar -tzf $1 | grep -v /$ | wc -l
-}
-
-
-
-tar -czf $output $input 2> /dev/null
-
-src_files=$( total_files $input )
-src_directories=$( total_directories $input )
-
-arch_files=$( total_archived_files $output )
-arch_directories=$( total_archived_directories $output )
-
-echo "Files to be included: $src_files"
-echo "Directories to be included: $src_directories"
-echo "Files archived: $arch_files"
-echo "Directories archived: $arch_directories"
-
-if [ $src_files -eq $arch_files ]; then
-        echo "Backup of $input completed!"
-        echo "Details about the output backup file:"
-        ls -l $output
-else
-        echo "Backup of $input failed!"
-fi
+for directory in $*; do
+        backup $directory 
+done
